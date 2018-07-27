94808ef49d4833f801dc146e735fab95859caa7f
Ville Hakulinen <ville.hakulinen@gmail.com>
2018-07-27 23:04:04 +0300

    Add support for ext_newgrid

--- src/neovim.rs
+++ src/neovim.rs
@@ -15,6 +15,7 @@ pub enum UiOption {
     ExtTabline(bool),
     ExtCmdline(bool),
     ExtWildmenu(bool),
+    ExtNewgrid(bool),
 }
 
 impl UiOption {
@@ -30,6 +31,7 @@ impl UiOption {
             &UiOption::ExtTabline(val) => ("ext_tabline", val.into()),
             &UiOption::ExtCmdline(val) => ("ext_cmdline", val.into()),
             &UiOption::ExtWildmenu(val) => ("ext_wildmenu", val.into()),
+            &UiOption::ExtNewgrid(val) => ("ext_newgrid", val.into()),
         }
     }
 }
@@ -76,6 +78,11 @@ impl UiAttachOptions {
 
     pub fn set_wildmenu_external(&mut self, wildmenu_external: bool) -> &mut Self {
         self.set_option(UiOption::ExtWildmenu(wildmenu_external));
+        self
+    }
+
+    pub fn set_newgrid_external(&mut self, newgrid_external: bool) -> &mut Self {
+        self.set_option(UiOption::ExtNewgrid(newgrid_external));
         self
     }
 
